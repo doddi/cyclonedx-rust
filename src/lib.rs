c63c15dbfba3b0ae815543e8ac53221eced01e44
markdodgson <mdodgson@sonatype.com>
2021-03-10 09:37:28 +0000

    initial add of both jsn and xml

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,21 @@
-use serde::{Deserialize, Serialize};
+use serde::{Serialize};
 use std::error::Error;
 use std::fmt;
 use std::fmt::Formatter;
+use yaserde_derive::YaSerialize;
+use yaserde::ser::Config;
+use std::time::SystemTime;
+use chrono::{DateTime, Utc};
 
+const XMLNS: &'static str = "http://cyclonedx.org/schema/bom/1.2";
 const BOM_FORMAT: &'static str = "CycloneDX";
 const SPEC_VERSION: &'static str = "1.2";
 const DEFAULT_VERSION: &'static str = "1";
+
+#[derive(PartialEq)]
+pub enum CycloneDXEncodeType {
+    XML, JSON
+}
 
 #[derive(Debug)]
 pub struct CycloneDXEncodeError {}
@@ -16,30 +26,105 @@ impl fmt::Display for CycloneDXEncodeError {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[yaserde(rename = "bom", rename_all = "camelCase")]
+#[derive(Default, PartialEq, Debug, YaSerialize)]
+pub struct XMLCycloneDX {
+    #[yaserde(rename = "serialNumber", attribute)]
+    serial_number: String,
+    #[yaserde(attribute)]
+    version: String,
+    #[yaserde(attribute)]
+    xmlns: String,
+
+    cyclonedx: CycloneDX
+}
+
+impl XMLCycloneDX {
+    pub fn new() -> XMLCycloneDX {
+        XMLCycloneDX {
+            serial_number: "urn:uuid:".to_owned() + &uuid::Uuid::new_v4().to_string(),
+            version: DEFAULT_VERSION.to_string(),
+            xmlns: XMLNS.to_string(),
+            cyclonedx: Default::default()
+        }
+    }
+}
+
+
 #[serde(rename_all = "camelCase")]
+#[derive(Default, PartialEq, Debug, Serialize)]
+pub struct JSONCycloneDX {
+    bom_format: String,
+    spec_version: String,
+    serial_number: String,
+    version: String,
+
+    cyclonedx: CycloneDX
+}
+
+impl JSONCycloneDX {
+    pub fn new() -> JSONCycloneDX {
+        JSONCycloneDX {
+            bom_format: BOM_FORMAT.to_string(),
+            spec_version: SPEC_VERSION.to_string(),
+            serial_number: "urn:uuid:".to_owned() + &uuid::Uuid::new_v4().to_string(),
+            version: DEFAULT_VERSION.to_string(),
+
+            cyclonedx: Default::default()
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Serialize, YaSerialize)]
+pub struct Metadata {
+    time_stamp: String
+}
+
+impl Metadata {
+    pub fn new() -> Metadata {
+        let time_stamp: DateTime<Utc> = SystemTime::now().into();
+        Metadata {
+            time_stamp: time_stamp.to_rfc3339()
+        }
+    }
+}
+// #[serde(rename = "bom", rename_all = "camelCase")]
+#[derive(Default, PartialEq, Debug, Serialize, YaSerialize)]
+#[yaserde(flatten)]
 pub struct CycloneDX {
-    bom_format: &'static str,
-
-    spec_version: &'static str,
-
-    version: &'static str,
+    metadata: Option<Metadata>,
 }
 
 impl CycloneDX {
     pub fn new() -> CycloneDX {
         CycloneDX {
-            bom_format: BOM_FORMAT,
-            spec_version: SPEC_VERSION,
-            version: DEFAULT_VERSION
+            metadata: None
         }
     }
 
-    pub fn encode<W>(&self, writer: W) -> Result<(), CycloneDXEncodeError>
+    pub fn encode<W>(writer: W, dx: CycloneDX, format: CycloneDXEncodeType) -> Result<(), CycloneDXEncodeError>
     where
         W: std::io::Write,
     {
-        let result = serde_json::to_writer_pretty(writer, self);
+        let result = match format {
+            CycloneDXEncodeType::XML => {
+                let mut xml: XMLCycloneDX = XMLCycloneDX::new();
+                xml.cyclonedx = dx;
+                let config: Config = Config {
+                    perform_indent: true,
+                    write_document_declaration: true,
+                    indent_string: None
+                };
+                yaserde::ser::serialize_with_writer(&xml, writer, &config);
+                Ok(())
+            }
+
+            CycloneDXEncodeType::JSON => {
+                let mut json: JSONCycloneDX = JSONCycloneDX::new();
+                json.cyclonedx = dx;
+                serde_json::to_writer_pretty(writer, &json)
+            }
+        };
 
         if result.is_err() {
             return Err(CycloneDXEncodeError {});
@@ -50,17 +135,16 @@ impl CycloneDX {
 
 #[cfg(test)]
 mod tests {
-    use crate::CycloneDX;
+    use crate::{CycloneDX, Metadata};
     use std::io::ErrorKind;
+    use crate::CycloneDXEncodeType::{JSON, XML};
 
-    #[test]
-    fn new_bom_has_defaults() {
-        let bom = CycloneDX::new();
-
-        assert_eq!(bom.bom_format, "CycloneDX");
-        assert_eq!(bom.spec_version, "1.2");
-        assert_eq!(bom.version, "1");
-    }
+    // #[test]
+    // fn new_bom_has_defaults() {
+    //     let bom = CycloneDX::new();
+    //
+    //     assert_eq!(bom.version, "1");
+    // }
 
     #[test]
     fn error_if_invalid_writer() {
@@ -78,7 +162,7 @@ mod tests {
 
         // Used to to get access to the dummy Write trait above
         let writer = Box::new(CycloneDX::new());
-        let result = cyclone_dx.encode(writer);
+        let result = CycloneDX::encode(writer, cyclone_dx, JSON);
 
         assert!(result.is_err());
     }
@@ -86,17 +170,21 @@ mod tests {
     #[test]
     fn can_serialize_json() {
         let mut vec = Vec::new();
-        let buf = Box::new(&mut vec);
-        let cyclone_dx = CycloneDX::new();
+        let mut cyclone_dx = CycloneDX::new();
+        cyclone_dx.metadata = Option::from(Metadata::new());
 
-        let result = cyclone_dx.encode(buf);
+        let result = CycloneDX::encode(&mut vec, cyclone_dx, JSON);
 
         let actual = String::from_utf8(vec).unwrap();
         let expected = r#"
         {
             "bomFormat": "CycloneDX",
             "specVersion": "1.2",
-            "version": "1"
+            "serialNumber": "urn:uuid:3e671687-395b-41f5-a30f-a58921a69b79",
+            "version": 1,
+            "metadata": {
+                "timestamp": "2020-04-07T07:01:00Z"
+            }
         }"#;
 
         assert!(result.is_ok());
@@ -104,6 +192,23 @@ mod tests {
             remove_all_whitespace(actual.as_ref()),
             remove_all_whitespace(expected)
         );
+    }
+
+    #[test]
+    fn can_serialize_xml() {
+        let mut vec = Vec::new();
+        let mut cyclone_dx = CycloneDX::new();
+        cyclone_dx.metadata = Option::from(Metadata::new());
+
+        let result = CycloneDX::encode(&mut vec, cyclone_dx, XML);
+
+        let actual = String::from_utf8(vec).unwrap();
+        let expected = r#"
+        <bom version="1">
+        <bom>"#;
+
+        assert!(result.is_ok());
+        assert_eq!(actual, expected);
     }
 
     fn remove_all_whitespace(s: &str) -> String {
